d79e0a99831bef46b0c4f3c1618f5043be79cf90
Jirapat Suwanjunee <Suwanjunee987@gmail.com>
2023-01-25 19:28:58 +0700

    first commit

---
+++ app/src/main/java/android/my/projecttictactoe/MainActivity.kt
@@ -0,0 +1,32 @@
+package android.my.projecttictactoe
+
+import android.content.Intent
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+var singleUser = false
+class MainActivity : AppCompatActivity() {
+    lateinit var singlePlayerBtn: Button
+    lateinit var multiPlayerBtn: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        singlePlayerBtn = findViewById(R.id.idBtnSinglePlayer)
+        multiPlayerBtn = findViewById(R.id.idBtnMultiPlayer)
+        singlePlayerBtn.setOnClickListener{
+            singleUser = true
+            startActivity(Intent(this, GamePlayActivity::class.java))
+
+
+        }
+
+        multiPlayerBtn.setOnClickListener{
+            singleUser = false
+            startActivity(Intent(this, GamePlayActivity::class.java))
+        }
+    }
+}
