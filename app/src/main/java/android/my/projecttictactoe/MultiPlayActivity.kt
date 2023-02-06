059af6f795514b4dbd64ce75fbb21afaba973b93
Jirapat Suwanjunee <Suwanjunee987@gmail.com>
2023-02-06 13:07:41 +0700

    Activity Multi game

--- app/src/main/java/android/my/projecttictactoe/MultiPlayActivity.kt
+++ app/src/main/java/android/my/projecttictactoe/MultiPlayActivity.kt
@@ -92,6 +92,37 @@ class MultiPlayActivity: AppCompatActivity() {
         }
 
     }
+
+    fun robot() {
+        val rnd = (1..9).random()
+        if(emptyCells.contains(rnd))
+            robot()
+        else {
+            val buttonselected : Button?
+            buttonselected = when(rnd) {
+                1 -> button
+                2 -> button2
+                3 -> button3
+                4 -> button4
+                5 -> button5
+                6 -> button6
+                7 -> button7
+                8 -> button8
+                9 -> button9
+                else -> {button}
+            }
+            emptyCells.add(rnd);
+            buttonselected.text = "O"
+            buttonselected.setTextColor(Color.parseColor("#D22BB804"))
+            player2.add(rnd)
+            buttonselected.isEnabled = false
+            var checkWinner = playerwinner()
+            if(checkWinner == 1)
+                Handler().postDelayed(Runnable { reset() } , 2000)
+
+        }
+    }
+
     // Find the winner between X or O
     fun playerwinner():Int {
 
@@ -191,7 +222,7 @@ class MultiPlayActivity: AppCompatActivity() {
         }
     }
 
-    fun robot() {
+ /*   fun robot() {
         val rnd = (1..9).random()
         if(emptyCells.contains(rnd))
             robot()
@@ -219,7 +250,7 @@ class MultiPlayActivity: AppCompatActivity() {
                 Handler().postDelayed(Runnable { reset() } , 2000)
 
         }
-    }
+    }*/
 
     fun buttonDisable()
     {
@@ -237,7 +268,6 @@ class MultiPlayActivity: AppCompatActivity() {
                 8 -> button8
                 9 -> button9
                 else -> {button}
-
             }
             if(buttonSelected.isEnabled == true)
                 buttonSelected.isEnabled = false
